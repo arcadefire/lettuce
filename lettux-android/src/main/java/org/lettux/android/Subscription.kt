4ccd86447c89bda36c6c221744106df5b7b102ee
Angelo Marchesin <angelo.marchesin@glovoapp.com>
2024-04-06 19:38:14 +0200

    Add StoreViewModel and Subscription

---
+++ lettux-android/src/main/java/org/lettux/android/Subscription.kt
@@ -0,0 +1,8 @@
+package org.lettux.android
+
+import kotlinx.coroutines.flow.Flow
+import org.lettux.core.Action
+
+fun interface Subscription<STATE: Any> {
+    fun subscribe(state: Flow<STATE>): Flow<Action>
+}
