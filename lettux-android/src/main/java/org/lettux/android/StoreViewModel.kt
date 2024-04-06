04ce70d3cd8a0b794d760b1f2df640909705da6a
Angelo Marchesin <marchesin.angelo@gmail.com>
2024-04-06 19:38:14 +0200

    Add StoreViewModel and Subscription

--- lettux-android/src/main/java/org/lettux/android/StoreViewModel.kt
+++ lettux-android/src/main/java/org/lettux/android/StoreViewModel.kt
@@ -1,9 +1,25 @@
 package org.lettux.android
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import org.lettux.core.ActionHandler
+import org.lettux.core.Middleware
 import org.lettux.core.Store
+import org.lettux.createStore
+import java.util.concurrent.Flow
 
-class StoreViewModel<STATE> constructor(
-    private val store: Store<STATE>
-) : ViewModel() {
+abstract class StoreViewModel<STATE : Any> constructor(
+    private val store: Store<STATE>,
+    private val subscription: Subscription<STATE>? = null,
+) : Store<STATE> by store, ViewModel() {
+
+    init {
+        subscription?.let {
+            subscription.subscribe(states)
+                .onEach { send(it) }
+                .launchIn(viewModelScope)
+        }
+    }
 }
